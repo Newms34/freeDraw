d609362676f261b2a6e0a5ad1c4542649b052af3
Newms34 <Newms3450@msn.com>
2016-05-30 17:48:44 -0400

    upd

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+// router.get('/',function(req,res,next){
+// 	res.sendFile('index.html',{"root":'./views'});
+// });
+// router.get('/admin',function(req,res,next){
+// 	res.sendFile('admin.html',{"root":'./views'});
+// });
+// router.get('/music',function(req,res,next){
+// 	res.sendFile('music.html',{"root":'./views'});
+// });
 
 router.get('/',function(req,res,next){
-	res.sendFile('index.html',{"root":'./views'});
+	res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 router.get('/admin',function(req,res,next){
-	res.sendFile('admin.html',{"root":'./views'});
+	res.sendFile(path.join(__dirname, '../views', 'admin.html'));
 });
 router.get('/music',function(req,res,next){
-	res.sendFile('music.html',{"root":'./views'});
+	res.sendFile(path.join(__dirname, '../views', 'music.html'));
 });
+
 router.get('/temp',function(req,res,next){
 	res.send('HI');
 });
+// res.sendFile(path.join(__dirname, '../public', 'index1.html'));
 module.exports=router;
